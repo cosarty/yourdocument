f3fb223715b3f64c54172ad4af127590d8cd7fc2
cosart <1398675906@qq.com>
2022-08-30 23:00:01 +0800

    ✨ feat: 添加题目表单组件完善

--- src/pages/AddQuestion/index.tsx
+++ src/pages/AddQuestion/index.tsx
@@ -1,8 +1,15 @@
+/* eslint-disable no-param-reassign */
+import AddMultipleOptions from '@/components/AddMultipleOptions';
+import AddSingleOptions from '@/components/AddSingleOptions';
 import RichTextEditor from '@/components/RichTextEditor';
 import SelectTag from '@/components/SelectTag';
 import { QUESTION_DIFFICULTY_ENUM, QUESTION_TYPE_ENUM } from '@/constant/question';
-import { ProForm, ProFormDependency, ProFormList, ProFormRadio } from '@ant-design/pro-components';
+import { pick } from '@/util/utils';
+import type { ProFormInstance } from '@ant-design/pro-components';
+import { ProForm, ProFormDependency, ProFormRadio } from '@ant-design/pro-components';
 import { Button, Card, Col, Row } from 'antd';
+import BraftEditor from 'braft-editor';
+import { useRef, useState } from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const formItemLayout = {
@@ -51,8 +58,33 @@ const submitFormLayout = {
 };
 
 const AddQuestion = () => {
+  const formRef = useRef<ProFormInstance>();
+  const [submitting, setSubmitting] = useState<boolean>(false);
+
   const doSubmit = async (values: Record<string, any>) => {
-    console.log('values: ', values);
+    if (values.type === 2) {
+      values[`params${values.type}`].answer = values[`params${values.type}`].answer.join(',');
+    }
+
+    if ([0, 1, 2].includes(values.type)) {
+      values = pick(values, [
+        'detail',
+        'difficulty',
+        `params${values.type}`,
+        'tags',
+        'title',
+        'type',
+      ]);
+      // 编码转换
+      values[`params${values.type}`].options = Object.fromEntries(
+        values[`params${values.type}`].options.map((op: string, index: number) => {
+          const letter = String.fromCharCode(65 + index);
+          return [letter, op];
+        }),
+      );
+      values.params = values[`params${values.type}`];
+    }
+    Object.keys(values).map((k: string) => k.search(/params\d/) !== -1 && delete values[k]);
   };
 
   return (
@@ -63,6 +95,7 @@ const AddQuestion = () => {
         <Row gutter={[24, 24]}>
           <Col lg={16} sm={24}>
             <ProForm
+              formRef={formRef}
               {...formItemLayout}
               labelAlign='left'
               scrollToFirstError
@@ -71,9 +104,9 @@ const AddQuestion = () => {
                 difficulty: 0,
               }}
               submitter={false}
-              onValuesChange={(value, allValue) => {
-                console.log('value, allValue: ', value, allValue);
-              }}
+              // onValuesChange={(value, allValue) => {
+              //   console.log('value, allValue: ', value, allValue);
+              // }}
               onFinish={doSubmit}
             >
               <ProFormRadio.Group
@@ -125,23 +158,123 @@ const AddQuestion = () => {
               <ProForm.Item
                 label='题目'
                 name='title'
-                rules={[{ required: true, message: '请输入标题' }]}
+                rules={[
+                  { required: true, message: '请输入标题' },
+                  {
+                    validator: (_, value) => {
+                      if (value && !BraftEditor.createEditorState(value).toText().trim()) {
+                        return Promise.reject('标题必须包含文字');
+                      }
+                      return Promise.resolve();
+                    },
+                  },
+                ]}
               >
                 <RichTextEditor placeholder='请输入题目，尽量准确清晰，不用刻意加粗' />
               </ProForm.Item>
               <ProFormDependency name={['type']}>
                 {({ type }) => {
                   return (
-                    [1, 2, 0].includes(type) && (
-                      <ProFormList name='p' label='题目选项' copyIconProps={false} min={1} max={7}>
-                        {(f, index, action) => {
-                          console.log(f, index, action);
-                          return (
-                            <>{type === 0 && <ProFormRadio.Group name={['arams', 'answer']} />}</>
-                          );
-                        }}
-                      </ProFormList>
-                    )
+                    <>
+                      {[1, 2, 0].includes(type) && (
+                        <>
+                          {type === 0 && (
+                            <ProForm.Item
+                              name='params0'
+                              label={'题目选项'}
+                              shouldUpdate
+                              rules={[
+                                {
+                                  validator: (
+                                    _: any,
+                                    value: { options: string[]; answer: string },
+                                  ) => {
+                                    if (!value.answer || !value.options.every((op) => !!op)) {
+                                      return Promise.reject('答案和选项不能为空');
+                                    }
+
+                                    return Promise.resolve();
+                                  },
+                                },
+                              ]}
+                            >
+                              <AddSingleOptions min={2} max={2} />
+                            </ProForm.Item>
+                          )}
+                          {type === 1 && (
+                            <ProForm.Item
+                              name='params1'
+                              label={'题目选项'}
+                              shouldUpdate
+                              rules={[
+                                {
+                                  validator: (
+                                    _: any,
+                                    value: { options: string[]; answer: string },
+                                  ) => {
+                                    if (!value.answer || !value.options.every((op) => !!op)) {
+                                      return Promise.reject('答案和选项不能为空');
+                                    }
+
+                                    return Promise.resolve();
+                                  },
+                                },
+                              ]}
+                            >
+                              <AddSingleOptions min={3} max={6} />
+                            </ProForm.Item>
+                          )}
+                          {type === 2 && (
+                            <ProForm.Item
+                              name='params2'
+                              label={'题目选项'}
+                              shouldUpdate
+                              rules={[
+                                {
+                                  validator: (
+                                    _: any,
+                                    value: { options: string[]; answer: string[] },
+                                  ) => {
+                                    if (
+                                      !value.answer.every((op) => !!op) ||
+                                      !value.options.every((op) => !!op)
+                                    ) {
+                                      return Promise.reject('答案和选项不能为空');
+                                    }
+
+                                    return Promise.resolve();
+                                  },
+                                },
+                              ]}
+                            >
+                              <AddMultipleOptions min={4} max={6} />
+                            </ProForm.Item>
+                          )}
+                        </>
+                      )}
+                      {type === 3 && (
+                        <ProForm.Item
+                          label='答案'
+                          name='reference'
+                          rules={[
+                            { required: true, message: '答案不能为空' },
+                            {
+                              validator: (_, value) => {
+                                if (
+                                  value &&
+                                  !BraftEditor.createEditorState(value).toText().trim()
+                                ) {
+                                  return Promise.reject('答案不能为空');
+                                }
+                                return Promise.resolve();
+                              },
+                            },
+                          ]}
+                        >
+                          <RichTextEditor placeholder='请输入解析，尽量准确清晰，不用刻意加粗' />
+                        </ProForm.Item>
+                      )}
+                    </>
                   );
                 }}
               </ProFormDependency>
@@ -159,10 +292,10 @@ const AddQuestion = () => {
                   type='primary'
                   htmlType='submit'
                   block
-                  // loading={submitting}
-                  // disabled={submitting || disabled}
+                  loading={submitting}
+                  disabled={submitting}
                 >
-                  {'提交中'}
+                  {submitting ? '提交中' : '提交'}
                 </Button>
               </ProForm.Item>
             </ProForm>
